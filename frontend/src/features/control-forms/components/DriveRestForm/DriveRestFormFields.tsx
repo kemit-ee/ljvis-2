a565c6f6054ca198f6839f0b70d82a8adce2002f
Siim Türk <siim.turk@askend.com>
2026-09-18 14:01:22 +0300

    sõidu ja puhukeajavormi Ei vasta nõuetele teksti fix, märkuste placeholderi lisamine

--- frontend/src/features/control-forms/components/DriveRestForm/DriveRestFormFields.tsx
+++ frontend/src/features/control-forms/components/DriveRestForm/DriveRestFormFields.tsx
@@ -999,6 +999,9 @@ export function DriveRestFormFields({
                     id={fieldId('tachographNotes')}
                     maxHeight="8rem"
                     label={t('forms.sp_form.tachographNotes')}
+                    placeholder={
+                      readOnly ? '' : t('common.enterNotesPlaceholder')
+                    }
                     value={formik.values.tachographNotes ?? ''}
                     input={{ maxLength: 4000 }}
                     onChange={(v) =>
